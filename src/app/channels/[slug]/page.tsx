6646b08296cec69aa54476bdf6985760418754af
MitinWarface <mitinwarface@gmail.com>
2025-12-07 04:44:59 +0300

    page

--- src/app/channels/[slug]/page.tsx
+++ src/app/channels/[slug]/page.tsx
@@ -13,7 +13,7 @@ export default function ChannelsMePage() {
 
       const { data, error } = await supabase!
         .from('friends')
-        .select('*, profiles!friends_friend_id_fkey(*)')
+        .select('*, profiles!inner(id, username, avatar_url)')
         .eq('user_id', user.id)
         .eq('status', 'accepted');
 
@@ -26,68 +26,35 @@ export default function ChannelsMePage() {
 
   return (
     <main className="container__133bf" aria-label="Друзья">
-      <section className="theme-dark theme-midnight images-dark container__9293f themed__9293f" aria-labelledby="uid_31" role="navigation">
-        <div className="upperContainer__9293f">
-          <div className="children__9293f scrollable__9293f themed__9293f">
-            <div className="iconWrapper__9293f">
-              <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" className=""></path>
-                <path fill="currentColor" d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z" className=""></path>
-              </svg>
-            </div>
-            <div className="titleWrapper__9293f">
-              <h1 className="defaultColor__4bd52 text-md/medium__13cf1 defaultColor__5345c title__9293f" id="uid_31" data-text-variant="text-md/medium">Друзья</h1>
-            </div>
-            <svg className="dot__9293f" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 4 4"><circle cx="2" cy="2" r="2" fill="currentColor"></circle></svg>
-            <div className="tabBar__133bf topPill_aa8da2" role="tablist" aria-orientation="horizontal" aria-label="Друзья">
-              <div className="item__133bf item_aa8da2 selected_aa8da2 themed_aa8da2" role="tab" aria-selected="true" aria-controls="online-tab" aria-disabled="false" tabIndex={0}>В сети</div>
-              <div className="item__133bf item_aa8da2 themed_aa8da2" role="tab" aria-selected="false" aria-disabled="false" tabIndex={-1}>Все</div>
-              <div className="item__133bf addFriend__133bf item_aa8da2 themed_aa8da2" role="tab" aria-selected="false" aria-disabled="false" tabIndex={-1} aria-label="Добавить в друзья"><span>Добавить в друзья</span></div>
-            </div>
-          </div>
-          <div className="toolbar__9293f">
-            <div className="inviteToolbar__133bf">
-              <div className="iconWrapper__9293f clickable__9293f" role="button" aria-label="Новый групповой чат" tabIndex={0}>
-                <svg x="0" y="0" className="icon__9293f" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                  <path d="M19 14a1 1 0 0 1 1 1v3h3a1 1 0 0 1 0 2h-3v3a1 1 0 0 1-2 0v-3h-3a1 1 0 0 1 0-2h3v-3a1 1 0 0 1 1-1Z" fill="currentColor" className=""></path>
-                  <path d="M20.76 12.57c.4.3 1.23.13 1.24-.37V12a10 10 0 1 0-18.44 5.36c.12.19.1.44-.04.61l-2.07 2.37A1 1 0 0 0 2.2 22h10c.5-.01.67-.84.37-1.24A3 3 0 0 1 15 16h.5a.5.5 0 0 0 .5-.5V15a3 3 0 0 1 4.76-2.43Z" fill="currentColor" className=""></path>
-                </svg>
-              </div>
-              <span style={{ display: 'none' }}></span>
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="tabBody__133bf">
-        <div className="peopleColumn__133bf" aria-labelledby="uid_32" role="tabpanel" id="online-tab" tabIndex={-1}>
-          <div className="searchBar__5ec2f">
-            <div className="container__5a838" data-layout="vertical">
-              <div className="control__5a838">
-                <div className="container__72c38" data-full-width="true">
-                  <div className="wrapper__72c38 container__0f084 md__0f084 text-md/normal__0f084 hasLeading__0f084" data-error="false" data-disabled="false">
-                    <div className="icon__0f084">
-                      <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24">
-                        <path fill="var(--icon-primary)" fillRule="evenodd" d="M15.62 17.03a9 9 0 1 1 1.41-1.41l4.68 4.67a1 1 0 0 1-1.42 1.42l-4.67-4.68ZM17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" clipRule="evenodd" className=""></path>
-                      </svg>
-                    </div>
-                    <input className="input__0f084" placeholder="Поиск" data-mana-component="text-input" aria-label="Поиск" id="«r1i»" aria-invalid="false" type="text" value="" name="" />
-                  </div>
+      <div className="base__133bf">
+        <div className="sidebar__133bf">
+          <nav className="guilds__133bf">
+            <div className="guilds__133bf">
+              <div className="guild__133bf" aria-label="Домой" role="button" tabIndex={0}>
+                <div className="childWrapper__133bf">
+                  <svg className="homeIcon__133bf" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="28" height="20" fill="none" viewBox="0 0 28 20">
+                    <path fill="currentColor" d="M20.6644 20C20.6644 20 21.3944 19.2656 21.3944 18.1992C21.3944 17.1328 20.6644 16.3984 20.6644 16.3984C20.6644 16.3984 19.9344 17.1328 19.9344 18.1992C19.9344 19.2656 20.6644 20 20.6644 20ZM14.7016 20C14.7016 20 15.4316 19.2656 15.4316 18.1992C15.4316 17.1328 14.7016 16.3984 14.7016 16.3984C14.7016 16.3984 13.9716 17.1328 13.9716 18.1992C13.9716 19.2656 14.7016 20 14.7016 20ZM8.7388 20C8.7388 20 9.4688 19.2656 9.4688 18.1992C9.4688 17.1328 8.7388 16.3984 8.7388 16.3984C8.7388 16.3984 8.0088 17.1328 8.0088 18.1992C8.0088 19.2656 8.7388 20 8.7388 20Z"></path>
+                    <path fill="currentColor" d="M2.6644 20C2.6644 20 3.3944 19.2656 3.3944 18.1992C3.3944 17.1328 2.6644 16.3984 2.6644 16.3984C2.6644 16.3984 1.9344 17.1328 1.9344 18.1992C1.9344 19.2656 2.6644 20 2.6644 20ZM26.6644 20C26.6644 20 27.3944 19.2656 27.3944 18.1992C27.3944 17.1328 26.6644 16.3984 26.6644 16.3984C26.6644 16.3984 25.9344 17.1328 25.9344 18.1992C25.9344 19.2656 26.6644 20 26.6644 20Z"></path>
+                    <path fill="currentColor" d="M0 10.1992C0 8.01424 1.01424 6.1992 2.6644 5.1328C4.31456 4.0664 6.6644 3.1992 9.9976 2.1992C13.3308 1.1992 16.6644 0.6656 20 0.6656C23.3356 0.6656 26.6692 1.1992 30.0024 2.1992C33.3356 3.1992 35.6854 4.0664 37.3356 5.1328C38.9858 6.1992 40 8.01424 40 10.1992V20H0V10.1992Z"></path>
+                  </svg>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="peopleList__5ec2f auto_d125d2 scrollerBase_d125d2" role="list" tabIndex={0} data-list-id="people-list" aria-orientation="vertical" style={{ overflow: 'hidden scroll', paddingRight: '0px' }}>
-            <div className="content_d125d2" style={{ height: '1290px' }}>
-              <div aria-hidden="true" style={{ height: '0px' }}></div>
-              <div className="sectionTitle__5ec2f">
-                <h2 className="title__1472a container__13cf1 header__13cf1 text-sm/medium__13cf1" id="uid_32">В сети&nbsp;— 20</h2>
-              </div>
-              <div className="peopleListItem_cc6179" role="listitem" data-list-item-id="people-list___placeholder" tabIndex={-1} style={{ height: '62px', opacity: 1 }}>
-                <div className="listItemContents_fc004c">
-                  <div className="userInfo__0a06e">
-                    <div className="text__0a06e">
-                      <div className="subtext__0a06e">
-                        <div className="text__19b6d">У вас нет друзей для отображения</div>
+          </nav>
+          <div className="panels__133bf">
+            <div className="container__133bf">
+              <div className="privateChannels__133bf">
+                <div className="scroller__133bf">
+                  <div className="content__133bf">
+                    <div className="channel__133bf" aria-label="Друзья" role="button" tabIndex={0}>
+                      <div className="layout__133bf">
+                        <div className="content__133bf">
+                          <svg className="icon__133bf" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"></path>
+                            <path fill="currentColor" d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2Zm12 10c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3Zm-9 4c0-.22.03-.42.06-.63C5.74 16.86 7.87 15 10 15s4.26 1.86 4.94 3.37c.03.2.06.41.06.63H6Zm8-7c0-.55-.45-1-1-1s-1 .45-1 1 .45 1 1 1 1-.45 1-1Z"></path>
+                          </svg>
+                          <div className="name__133bf">Друзья</div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -96,34 +63,93 @@ export default function ChannelsMePage() {
             </div>
           </div>
         </div>
-        <div className="nowPlayingColumn__133bf">
-          <aside className="container__7d20c" aria-labelledby="uid_33">
-            <div className="scroller__7d20c thin_d125d2 scrollerBase_d125d2" dir="ltr" style={{ overflow: 'hidden scroll', paddingRight: '8px' }}>
-              <div className="">
-                <h2 className="defaultColor__4bd52 heading-lg/semibold_cf4812 defaultColor__5345c header__7d20c" id="uid_33" data-text-variant="heading-lg/semibold">Активные контакты</h2>
-                <div className="itemCard__7e549 wrapper__00943 outer_bf1984 padded_bf1984 interactive_bf1984" tabIndex={0} aria-expanded="false" aria-haspopup="menu" role="button">
-                  <div>
-                    <header className="headerFull__00943 header__00943">
-                      <div className="wrapper__44b0c headerAvatar__00943" role="img" aria-label="placeholder" aria-hidden="false" style={{ width: '32px', height: '32px' }}>
-                        <svg width="40" height="40" viewBox="0 0 40 40" className="mask__44b0c svg__44b0c" aria-hidden="true">
-                          <foreignObject x="0" y="0" width="32" height="32" mask="url(#svg-mask-avatar-default)">
-                            <div className="avatarStack__44b0c">
-                              <img alt=" " className="avatar__44b0c" aria-hidden="true" src="/assets/788f05731f8aa02e.png" />
+        <div className="content__133bf">
+          <div className="chat__133bf">
+            <div className="title__133bf">
+              <div className="children__133bf">
+                <h3 className="titleText__133bf">Друзья</h3>
+              </div>
+            </div>
+            <div className="content__133bf">
+              <div className="friendsTable__133bf">
+                <div className="friendsTableHeader__133bf">
+                  <div className="friendsTableHeaderInner__133bf">
+                    <div className="tabBar__133bf">
+                      <div className="tabBarItem__133bf selected__133bf" role="tab" aria-selected="true" tabIndex={0}>
+                        <div className="item__133bf">В сети — {friends.length}</div>
+                      </div>
+                      <div className="tabBarItem__133bf" role="tab" aria-selected="false" tabIndex={0}>
+                        <div className="item__133bf">Все</div>
+                      </div>
+                      <div className="tabBarItem__133bf" role="tab" aria-selected="false" tabIndex={0}>
+                        <div className="item__133bf">Ожидание</div>
+                      </div>
+                      <div className="tabBarItem__133bf" role="tab" aria-selected="false" tabIndex={0}>
+                        <div className="item__133bf">Заблокированные</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="friendsTableBody__133bf">
+                  <div className="scroller__133bf">
+                    <div className="content__133bf">
+                      {friends.length === 0 ? (
+                        <div className="emptyState__133bf">
+                          <div className="emptyStateImage__133bf">
+                            <svg className="image__133bf" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="376" height="162" fill="none" viewBox="0 0 376 162">
+                              <path fill="currentColor" d="M0 0h376v162H0z" opacity=".1"></path>
+                              <path fill="currentColor" d="M188 81c-20.5 0-37-16.5-37-37s16.5-37 37-37 37 16.5 37 37-16.5 37-37 37Zm0-55c-11.6 0-21 9.4-21 21s9.4 21 21 21 21-9.4 21-21-9.4-21-21-21Z"></path>
+                              <path fill="currentColor" d="M188 162c-41.4 0-75-33.6-75-75 0-8.3 6.7-15 15-15s15 6.7 15 15c0 24.8 20.2 45 45 45s45-20.2 45-45c0-8.3 6.7-15 15-15s15 6.7 15 15c0 41.4-33.6 75-75 75Z"></path>
+                            </svg>
+                          </div>
+                          <div className="emptyStateText__133bf">
+                            У вас нет друзей, которые сейчас в сети.
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="friendsTableList__133bf">
+                          {friends.map((friend) => (
+                            <div key={friend.id} className="friendsTableRow__133bf">
+                              <div className="friendsTableRowInner__133bf">
+                                <div className="avatar__133bf">
+                                  <img src={friend.profiles?.avatar_url || '/default-avatar.png'} alt={friend.profiles?.username} />
+                                </div>
+                                <div className="name__133bf">
+                                  <div className="discriminator__133bf">{friend.profiles?.username}</div>
+                                </div>
+                                <div className="actions__133bf">
+                                  <button className="actionButton__133bf">Сообщение</button>
+                                  <button className="actionButton__133bf">Ещё</button>
+                                </div>
+                              </div>
                             </div>
-                          </foreignObject>
-                        </svg>
-                      </div>
-                      <div>
-                        <div className="defaultColor__4bd52 text-md/semibold_cf4812 textContent__00943" data-text-variant="text-md/semibold">Нет активных контактов</div>
-                      </div>
-                    </header>
+                          ))}
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </aside>
+          </div>
+          <div className="members__133bf">
+            <div className="membersWrap__133bf">
+              <div className="members__133bf">
+                <div className="container__133bf">
+                  <div className="scroller__133bf">
+                    <div className="content__133bf">
+                      <h2 className="title__133bf">Активные контакты</h2>
+                      <div className="list__133bf">
+                        {/* Active friends or something */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
